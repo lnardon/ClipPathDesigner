1228611a51167a61d38b3787991524a079b5a59a
lnardon <lucaslnardon@gmail.com>
2021-04-29 20:27:07 -0300

    🚧 Add base functionality structure

---
+++ index.js
@@ -0,0 +1,18 @@
+let currentNode;
+let nodeList = [];
+let drawAreaWidth = document
+  .getElementsByClassName("clipPathDiv")[0]
+  .getBoundingClientRect().width;
+let drawAreaHeight = document
+  .getElementsByClassName("clipPathDiv")[0]
+  .getBoundingClientRect().height;
+
+function addNode() {}
+
+function deleteNode(index) {}
+
+function copyCSS() {}
+
+function webVersion() {}
+
+function mobileVersion() {}
