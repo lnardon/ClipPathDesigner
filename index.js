94d3df3f58f0ab7509b84cecbc2e0d2411346031
lnardon <lucaslnardon@gmail.com>
2021-04-30 22:59:05 -0300

    ✨ Finish deleteNode function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let currentNode;
+let currentNode = { id: 1 };
 let nodeList = [];
 let drawAreaWidth = document
   .getElementsByClassName("clipPathDiv")[0]
@@ -9,13 +9,18 @@ let drawAreaHeight = document
 
 function addNode() {
   let div = document.createElement("div");
-  div.className = "handle";
+  div.className = `handle ${nodeList.length}`;
   div.setAttribute("nodeId", nodeList.length);
   document.getElementsByClassName("clipPathDiv")[0].append(div);
   nodeList.push(div);
 }
 
-function deleteNode(index) {}
+function deleteNode() {
+  if (currentNode) {
+    nodeList.splice(currentNode.id, 1);
+    document.getElementsByClassName(`handle ${currentNode.id}`)[0].remove();
+  }
+}
 
 function copyCSS() {}
 
