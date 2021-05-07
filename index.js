466d172577aa39c51bc57ad5523b70255859d796
lnardon <lucaslnardon@gmail.com>
2021-05-07 04:25:04 -0300

    🚧 Add base drag and drop structure

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
+const drawArea = document.getElementsByClassName("clipPathDiv")[0];
 let currentNode = { id: 1 };
-let currentPath = "polygon(50% 0%, 0% 100%, 100% 100%)";
+let currentPath = "50% 0%, 0% 100%, 100% 100%";
 let nodeList = [];
-let drawArea = document.getElementsByClassName("clipPathDiv")[0];
-let drawAreaWidth = document
-  .getElementsByClassName("clipPathDiv")[0]
-  .getBoundingClientRect().width;
-let drawAreaHeight = document
-  .getElementsByClassName("clipPathDiv")[0]
-  .getBoundingClientRect().height;
 
 function addNode() {
   let div = document.createElement("div");
   div.className = `handle ${nodeList.length}`;
   div.setAttribute("nodeId", nodeList.length);
+  div.setAttribute("draggable", true);
+  div.setAttribute("ondragstart", "startDrag()");
+  div.setAttribute("ondrop", "dropHandle()");
   document.getElementsByClassName("clipPathDiv")[0].append(div);
   nodeList.push(div);
 }
 
 function deleteNode() {
-  if (currentNode) {
+  if (nodeList.length > 1) {
     nodeList.splice(currentNode.id, 1);
     document.getElementsByClassName(`handle ${currentNode.id}`)[0].remove();
   }
@@ -35,8 +32,24 @@ function copyCSS() {
 
 function webVersion() {
   drawArea.style.width = "600px";
+  document
+    .getElementsByClassName("deviceBtn")[1]
+    .classList.remove("btn-active");
+  document.getElementsByClassName("deviceBtn")[0].classList.add("btn-active");
 }
 
 function mobileVersion() {
   drawArea.style.width = "250px";
+  document
+    .getElementsByClassName("deviceBtn")[0]
+    .classList.remove("btn-active");
+  document.getElementsByClassName("deviceBtn")[1].classList.add("btn-active");
+}
+
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+function drag(e) {
+  document.getElementsByClassName("handle")[0].style.marginLeft = "200px";
 }
