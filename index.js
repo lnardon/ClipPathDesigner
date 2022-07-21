8bb8a27e4e2f93f98bd1001a29cce2cb63df5054
Lucas Nardon <lucaslnardon@gmail.com>
2022-07-21 19:13:22 -0300

    ✨ Add handle position functionality

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const drawArea = document.getElementsByClassName("clipPathDiv")[0];
-let currentNode = { id: 1 };
+let currentNode = { id: 0 };
 let currentPath = "50% 0%, 0% 100%, 100% 100%";
 let nodeList = [];
 
+document
+  .getElementsByClassName("handles")[0]
+  .addEventListener("dragover", (evt) => {
+    dropHandle(evt);
+  });
+
 function addNode() {
   let div = document.createElement("div");
-  div.className = `handle ${nodeList.length}`;
-  div.setAttribute("nodeId", nodeList.length);
   div.setAttribute("draggable", true);
-  div.setAttribute("ondragstart", "startDrag()");
-  div.setAttribute("ondrop", "dropHandle()");
-  document.getElementsByClassName("clipPathDiv")[0].append(div);
+  div.setAttribute("ondrag", "drag(event)");
+  div.setAttribute("drop", "dropHandle(event)");
+  div.setAttribute("class", `handle ${nodeList.length}`);
+  document.getElementsByClassName("handles")[0].append(div);
   nodeList.push(div);
 }
 
@@ -46,10 +51,70 @@ function mobileVersion() {
   document.getElementsByClassName("deviceBtn")[1].classList.add("btn-active");
 }
 
-function allowDrop(e) {
-  e.preventDefault();
+function drag(e) {
+  let t = drawArea.getBoundingClientRect();
+  document.getElementsByClassName(
+    "box"
+  )[0].style.clipPath = `polygon(${Mapalizer(
+    e.clientX,
+    t.left,
+    t.left + t.width,
+    0,
+    100,
+    0
+  )}% ${Mapalizer(
+    e.clientY,
+    t.top,
+    t.top + t.height,
+    0,
+    100,
+    0
+  )}%, 0% 100%, 100% 100%)`;
 }
 
-function drag(e) {
-  document.getElementsByClassName("handle")[0].style.marginLeft = "200px";
+function dropHandle(e) {
+  e.preventDefault();
+  let t = drawArea.getBoundingClientRect();
+  document.getElementsByClassName(
+    `handle ${currentNode.id}`
+  )[0].style.left = `${Mapalizer(
+    e.clientX,
+    t.left,
+    t.left + t.width,
+    0,
+    100,
+    1
+  )}%`;
+  document.getElementsByClassName(
+    `handle ${currentNode.id}`
+  )[0].style.top = `${Mapalizer(
+    e.clientY,
+    t.top,
+    t.top + t.height,
+    0,
+    100,
+    1
+  )}%`;
+  console.log(e.clientY);
+}
+
+// HELPER
+function Mapalizer(
+  value,
+  initalBaseVal,
+  finalBaseVal,
+  initalTargetVal,
+  finalTargetVal,
+  precision
+) {
+  let result = 0;
+  result =
+    ((value - initalBaseVal) / (finalBaseVal - initalBaseVal)) *
+      (finalTargetVal - initalTargetVal) +
+    initalTargetVal;
+
+  if (precision !== 0) {
+    result = parseFloat(result.toFixed(precision));
+  }
+  return result;
 }
