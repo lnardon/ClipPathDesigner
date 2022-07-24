48966435c299945327d057a8fdb4dd9512eff308
Lucas Nardon <lucaslnardon@gmail.com>
2022-07-24 01:38:57 -0300

    ✨ Add uuid to each handle

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const drawArea = document.getElementsByClassName("clipPathDiv")[0];
+let t = drawArea.getBoundingClientRect();
 let currentNode = { id: 0 };
 let currentPath = "50% 0%, 0% 100%, 100% 100%";
 let nodeList = [{ id: 0 }, { id: 1 }, { id: 2 }];
@@ -10,20 +11,24 @@ document
   });
 
 function addNode() {
+  let uuid = Math.pow(Date.now() * Math.random(), Math.random());
   let div = document.createElement("div");
   div.setAttribute("draggable", true);
   div.setAttribute("ondrag", "drag(event)");
-  div.setAttribute("class", `handle ${nodeList.length}`);
+  div.setAttribute("class", `handle ${uuid}`);
   document.getElementsByClassName("handles")[0].append(div);
-  nodeList.push(div);
+  nodeList.push({ id: uuid });
   let t = drawArea.getBoundingClientRect();
   generatePath({ clientX: 0, clientY: 0 }, t);
 }
 
-function deleteNode() {
-  if (nodeList.length > 1) {
+function deleteNode(e) {
+  if (nodeList.length > 3) {
     nodeList.splice(currentNode.id, 1);
     document.getElementsByClassName(`handle ${currentNode.id}`)[0].remove();
+    generatePath(e, t);
+  } else {
+    alert("At least 3 handles should be present");
   }
 }
 
@@ -55,7 +60,6 @@ function mobileVersion() {
 
 function drag(e) {
   currentNode = { id: e.target.classList[1] };
-  let t = drawArea.getBoundingClientRect();
   generatePath(e, t);
 }
 
@@ -86,19 +90,24 @@ function dropHandle(e) {
 
 function generatePath(e, t) {
   let nodes = currentPath.split(",");
-  nodes[e.target.classList[1]] = `${Mapalizer(
-    e.clientX,
-    t.left,
-    t.left + t.width,
-    0,
-    100,
-    1
-  )}% ${Mapalizer(e.clientY, t.top, t.top + t.height, 0, 100, 1)}%`;
-  let updatedPath = nodes.join(",");
-  document.getElementsByClassName(
-    "box"
-  )[0].style.clipPath = `polygon(${updatedPath})`;
-  currentPath = updatedPath;
+  for (const i in nodeList) {
+    if (nodeList[i].id == e.target.classList[1]) {
+      console.log(nodes[i], e.target.classList[1]);
+      nodes[i] = `${Mapalizer(
+        e.clientX,
+        t.left,
+        t.left + t.width,
+        0,
+        100,
+        1
+      )}% ${Mapalizer(e.clientY, t.top, t.top + t.height, 0, 100, 1)}%`;
+      let updatedPath = nodes.join(",");
+      document.getElementsByClassName(
+        "box"
+      )[0].style.clipPath = `polygon(${updatedPath})`;
+      currentPath = updatedPath;
+    }
+  }
 }
 
 // HELPER
